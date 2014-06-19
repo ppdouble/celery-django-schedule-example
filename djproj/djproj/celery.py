25a9d33cedcc0a935604ccef296957631ac74c86
nlucky <ningning.dong@dianping.com>
2014-06-19 16:19:32 +0800

    new prj

---
+++ djproj/djproj/celery.py
@@ -0,0 +1,32 @@
+from __future__ import absolute_import
+
+import os
+import datetime
+from celery import Celery
+
+from django.conf import settings
+
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djproj.settings')
+
+app = Celery('djproj')
+
+# Using a string here means the worker will not have to
+# pickle the object when using Windows.
+app.config_from_object('django.conf:settings')
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
+
+@app.task
+def defaulttask1():
+    currt = datetime.datetime.now()
+    with open("files/defaulttask1.txt", "w") as fo:
+        print >> fo, currt.isoformat()+"default_task_1"
+        return currt 
+
+@app.task
+def hello():
+    print "helloincelery" 
---
+++ djproj/run.sh
@@ -0,0 +1,1 @@
+celery -A djproj worker -B -l debug
